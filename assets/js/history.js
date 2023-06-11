c0283af814dffdecf4087fa36eda0c17ffa755a5
Liam-WB <>
2023-06-11 19:11:33 +0000

    -Add minor history.js code changes

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,102 +1,28 @@
 // History category
 // Event listener, waits for DOM content
-
-
+// QList - compiles all question functions
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
+    let QList = [Q1(), Q2(), Q3(), Q4(), Q5(), Q6(), Q7(), Q8()];
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "submit") {
-                checkAnswer();
-                runGame(mathList[Math.floor(Math.random() * mathList.length)]);
-            } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
-            }
+            checkAnswer();
+            QList[Math.floor(Math.random() * QList.length)];
         });
     }
 
     document.getElementById("answer-box").addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             checkAnswer();
-            runGame(mathList[Math.floor(Math.random() * mathList.length)]);
+            QList[Math.floor(Math.random() * QList.length)];
         }
     });
 
-    runGame(mathList[Math.floor(Math.random() * mathList.length)]);
+    Q3();
+
 });
-
-function runGame(gameType) {
-
-    // .floor rounds the number, .random generates random number - (between 0 - 25) * 2
-    // Throws error code for unrecognised "gameType"
-
-    document.getElementById("answer-box").value = "";
-    document.getElementById("answer-box").focus();
-
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
-
-    if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2);
-    } else if (gameType === "multiply") {
-        displayMultiplyQuestion(num1, num2);
-    } else if (gameType === "subtract") {
-        displaySubtractQuestion(num1, num2);
-    } else if (gameType === "division") {
-        displayDivisionQuestion(num1, num2);
-    } else {
-        alert(`Error: Unknown ${gameType}`);
-        throw `Error: Unknown ${gameType}, aborting`;
-    }
-
-}
-
-function checkAnswer() {
-
-    // Compare user/s written answer against correct answer
-    // If statement returns result
-
-    let userAnswer = parseInt(document.getElementById("answer-box").value);
-    let calculatedAnswer = calculateCorrectAnswer();
-    let isCorrect = userAnswer === calculatedAnswer[0];
-
-    if (isCorrect) {
-        alert("Correct!");
-        incrementScore();
-    } else {
-        alert(`${userAnswer} is incorrect! The answer is ${calculatedAnswer[0]}.`);
-        incrementWrongAnswer();
-    }
-
-    runGame(calculatedAnswer[1]);
-
-}
-
-function calculateCorrectAnswer() {
-
-    // Attains integer 1 and 2 and mathematical operator from DOM
-    // Throws error code for unrecognised "operator"
-
-    let operand1 = parseInt(document.getElementById("operand1").textContent);
-    let operand2 = parseInt(document.getElementById("operand2").textContent);
-    let operator = document.getElementById("operator").textContent;
-
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-    } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"];
-    } else if (operator === "-") {
-        return [operand1 - operand2, "subtract"];
-    } else if (operator === "/") {
-        return [operand1 / operand2, "division"];
-    } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}, aborting!`;
-    }
-}
 
 function incrementScore() {
 
@@ -113,5 +39,106 @@ function incrementWrongAnswer() {
 
     let oldScore = parseInt(document.getElementById("wrong-score").innerText);
     document.getElementById("wrong-score").innerText = ++oldScore;
+
+}
+
+// History question content
+
+function Q1() {
+
+    document.getElementById("question").innerHTML = "Which war was also known as the Great War?";
+
+}
+
+function Q2() {
+
+    document.getElementById("question").innerHTML = "Who discovered the Americas in 1492?";
+
+}
+
+function Q3() {
+
+    document.getElementById("question").innerHTML = "What year was the great fire of London?";
+
+}
+
+function Q4() {
+
+    document.getElementById("question").innerHTML = "Who was the leader of Soviet Russia during WWII?";
+
+}
+
+function Q5() {
+
+    document.getElementById("question").innerHTML = "Was Pompeii was destroyed by an eruption of Mount Etna?";
+
+}
+
+function Q6() {
+
+    document.getElementById("question").innerHTML = "Apollo, Athena and Poseidon are part of which type of mythology?";
+
+}
+
+function Q7() {
+
+    document.getElementById("question").innerHTML = "Who was President when the 9/11 attacks took place?";
+
+}
+
+function Q8() {
+
+    document.getElementById("question").innerHTML = "Who was the first president of the USA?";
+
+}
+
+function calculateCorrectAnswer() {
+
+    // Attains integer 1 and 2 and mathematical operator from DOM
+    // Throws error code for unrecognised "operator"
+
+    let questionContent = document.getElementById("question").innerHTML;
+
+    if (questionContent = "Which war was also known as the Great War ?") {
+        return "World War 1";
+    } else if (questionContent = "Who discovered the Americas in 1492?") {
+        return "Christopher Columbus";
+    } else if (questionContent = "What year was the great fire of London?") {
+        return "1666";
+    } else if (questionContent = "Who was the leader of Soviet Russia during WWII?") {
+        return "Joseph Stalin";
+    } else if (questionContent = "True Or False: Pompeii was destroyed by an eruption of Mount Etna") {
+        return "False";
+    } else if (questionContent = "Who was the first man on the moon?") {
+        return "Neil Armstrong";
+    } else if (questionContent = "Who was President when the 9/11 attacks took place?") {
+        return "George Bush";
+    } else if (questionContent = "Who was the first president of the USA?") {
+        return "George Washington";
+    } else {
+        alert(`Unimplemented operator ${questionContent}`);
+        throw `Unimplemented operator ${questionContent}, aborting!`;
+    }
+}
+
+// Compare answers
+
+function checkAnswer() {
+
+    // Compare user/s written answer against correct answer
+    // If statement returns result
+
+    let userAnswer = document.getElementById("answer-box").value;
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer == calculatedAnswer;
+
+    if (isCorrect) {
+        alert("Correct!");
+        incrementScore();
+    } else {
+        alert(`${userAnswer} is incorrect! The answer is ${calculatedAnswer}.`);
+        incrementWrongAnswer();
+        console.log(calculatedAnswer);
+    }
 
 }
